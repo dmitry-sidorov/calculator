9fa1a745ef7f42e1c6b43869b7d80d01353fe4d2
kvatoferma <jimmaggot@mail.ru>
2018-04-26 14:33:34 +0300

    Add keyAnalize() to transform keyboard input into string variable

--- script.js
+++ script.js
@@ -1,13 +1,114 @@
 let str = '513+10*31-40/25';
-let variable = "";
-let currentOperator = "";
 
 
+let operandArray = [];
+let operatorArray = [];
+let buffer = "";
+for (let i = 0; i < str.length; i++) {
+	if (isDigit(str[i])) {
+		buffer += str[i];
+	} else {
+		operandArray.push(buffer);
+		buffer = "";
+		operatorArray.push(str[i]);
+	}
+}
+
+
+
+
+console.log(str);
+console.log(operandArray);
+console.log(operatorArray);
+
+
+// let test1 = "1234567890";
+// let test2 = "qwerty";
+
+// // function testFun(testValue) {
+// // 	for (let i = 0; i < testValue.length; i++){
+// // 		console.log({testValue[i]: numberCheck(testValue[i])});
+// // 	}
+// // }
+
+// // testFun(test1);
+// // testFun(test2);
+// console.log(test1, numberCheck(test1));
+// console.log(test2, numberCheck(test2));
+
+let inputString = "";
 
 let numberString = "";
-document.addEventListener('keypress',(e) => {
-	numberString += e.key;
+document.addEventListener('keydown',(e) => {
+	console.log(keyAnalize(e.code));
+	console.log(e.code);
+	inputString += keyAnalize(e.code);
+	
+	// console.log(typeof result);
+	// numberString += e.code;
+	// console.log(typeof e.code);
 });
+
+function keyAnalize(key) {
+	switch (key) {
+		case "Digit0": 
+		case "Numpad0": return "0";
+		break;
+		case "Digit1":
+		case "Numpad1": return "1";
+		break;
+		case "Digit2":
+		case "Numpad2": return "2";
+		break;
+		case "Digit3":
+		case "Numpad3": return "3";
+		break;
+		case "Digit4":
+		case "Numpad4": return "4";
+		break;
+		case "Digit5":
+		case "Numpad5": return "5";
+		break;
+		case "Digit6":
+		case "Numpad6": return "6";
+		break;
+		case "Digit7":
+		case "Numpad7": return "7";
+		break;
+		case "Digit8":
+		case "Numpad8": return "8";
+		break;
+		case "Digit9":
+		case "Numpad9": return "9";
+		break;
+		case "NumpadDivide": return "/";
+		break;
+		case "NumpadMultiply": return "*";
+		break;
+		case "NumpadSubtract": return "-";
+		break;
+		case "NumpadAdd": return "+";
+		break;
+		case "NumpadEnter":
+		case "Enter": return "=";
+		break;
+		case "NumpadDecimal":
+		case "Period":
+		case "Comma": return ".";
+		break;
+		case "Backspace": return "b";
+		break;
+		case "Delete": return "d";
+		break;
+		default: return "";
+	}
+}
+
+function expParse(expression) {
+	
+}
+
+
 
 
 
@@ -61,6 +162,10 @@ function multiply(a, b) {
 function divide(a, b) {
 	if (b === 0) return "Dividing by 0 error!";
 	else return (a / b);
+}
+function isDigit(num) {
+	if (/\d/g.test(num)) return true;
+	else if (/\D/g.test(num)) return false;
 }
 
 
